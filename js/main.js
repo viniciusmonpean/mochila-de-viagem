65b0e177576500f0b2c8bab0851ba5de852e9d76
Fausto Monpean <faustodesenvolvedor@gmail.com>
2022-12-12 19:21:06 -0300

    Carregamento de lista pelo localstorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const formulario = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+ 
+itens.forEach(element => {
+    criaElemento(element.nome, element.quantidade)
+});
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault()
@@ -33,6 +37,6 @@ function criaElemento(nome, quantidade) {
 
     itens.push(itemAtual)
 
-    localStorage.setItem("item", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
     
 }
