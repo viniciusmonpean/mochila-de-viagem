61fc276dde5abad9a981316ad08193539d186b69
Fausto Monpean <faustodesenvolvedor@gmail.com>
2022-12-13 22:19:04 -0300

    Estilização do design para ficar responsivo e finalização das funções da mochila (adicionar, atualizar e apagar)

--- js/main.js
+++ js/main.js
@@ -11,14 +11,26 @@ formulario.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(element => element.nome === nome.value)
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(element => element.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0;
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -32,12 +44,35 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
     
     novoItem.innerHTML += item.nome
     
-    lista.appendChild(novoItem)
-    
-    
-    
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)   
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(element => element.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
